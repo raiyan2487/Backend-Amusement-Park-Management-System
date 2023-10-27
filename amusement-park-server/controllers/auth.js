597187dfa1d3b1a5c61991de7aedc05d57e2a077
raiyan2487 <90189775+raiyan2487@users.noreply.github.com>
2023-10-27 16:33:17 +0600

    Added adminlogin

--- amusement-park-server/controllers/auth.js
+++ amusement-park-server/controllers/auth.js
@@ -3,6 +3,70 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const db = require("../db");
 
+exports.AdminLogin = (req, res) => {
+        const { UserID, password } = req.body;
+    
+        // Sanitize and escape inputs to prevent SQL injection
+        const sanitizedUserID = db.escape(UserID);
+    
+        // Query the database
+        const query = `SELECT * FROM admin WHERE UserID = ${sanitizedUserID}`;
+        db.query(query, async (err, results) => {
+            if (err) {
+                console.error('Database error:', err);
+                return res.status(500).send('Internal server error');
+            }
+    
+            // Check if a user with the provided username exists
+            if (results.length === 0) {
+                return res.status(200).send('User not found');
+            }
+    
+            const user = results[0]; 
+    
+    
+            // Compare password with the stored hash (you need to hash the password during registration)
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+            let token
+            if (isPasswordValid) {
+                // Authentication part
+                token = jwt.sign({ name: user.username }, process.env.ACCESS_TOKEN_SECRET, {
+                    expiresIn: '24h'
+                });
+                db.query('UPDATE admin SET token = ? WHERE username = ?', [token, user.username], (updateErr) => {
+                    if (updateErr) {
+                        console.error('Token update error:', updateErr);
+                        return res.status(500).send('Token update error');
+                    }
+
+                    res.status(200).json({ message: 'Login successful', token }); 
+                });
+    
+            } else {
+                return res.status(400).send('Invalid password');
+            }
+            
+    
+            // const query = 'SELECT * FROM users where token = ?', [token];
+            db.query('SELECT * FROM users where token = ?', [token], (err, results) => {
+                if (err) {
+                    console.error('User data can not be found:', err);
+    
+                }
+                else {
+                    db.query('SELECT * FROM users where token = ?', [token], (err, results) => {
+                        if(err) throw new Error()
+    
+                        console.log(results)
+                    })
+                }
+            })
+    
+        });  
+        
+    };
+
+
 exports.Register = (req, res) => {
         console.log(req.body);
     
